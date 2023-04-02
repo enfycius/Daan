c05a66f6abf51f35805977517fdecc66423c65ce
enfycius <hexk0131@gmail.com>
2023-04-02 15:06:51 +0900

    23.04.02

---
+++ Python/1-2.py
@@ -0,0 +1,42 @@
+# 문자열(String)
+
+# cf) 숫자형 (Number)
+
+# 문자: 문자 하나를 의미 예: 'a'
+
+# 문자열: 문자 두 개 이상을 의미 예: 'Hello'
+
+"Life is too short, You need Python"
+
+'Life is too short, You need Python'
+
+# 큰따옴표를 사용하든, 작은따옴표를 사용하든 상관 없어요 !
+# 큰따옴표를 3개를 연속으로 써서 양쪽 둘러싸도 문자열이에요.
+
+"""Life is too short, You need Python"""
+
+# 작은따옴표를 3개를 연속으로 써서 양쪽 둘러싸도 문자열이에요.
+
+'''Life is too short, You need Python'''
+
+a = "Life is too short, You need Python"
+
+print(a)
+
+b = '"Python is very easy." he says.'
+
+print(b)
+
+c = "'Python is very easy.' he says."
+
+print(c)
+
+# 큰따옴표나 작은따옴표 앞에 백슬래시(\)를 작성하게 되면, 이것은 문자열의 시작과 끝을 나타내는 용도로 사용되는 것이
+# 아니라 단순히 그 문자를 출력해주는 용도로 사용되는 거에요,,,
+
+d = "\"Python is very easy.\" he says."
+
+print(d)
+
+
+# "파이썬이 정말 쉬워"라고 다안 학생이 말해줬어요,,,
